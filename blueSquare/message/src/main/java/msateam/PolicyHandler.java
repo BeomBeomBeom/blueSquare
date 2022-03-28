e7884b5649b5215fc423e026dc7faf0545a02e47
SeunghyeonJayJeon <102270647+SeunghyeonJayJeon@users.noreply.github.com>
2022-03-28 19:04:14 +0900

    Update PolicyHandler.java

--- blueSquare/message/src/main/java/msateam/PolicyHandler.java
+++ blueSquare/message/src/main/java/msateam/PolicyHandler.java
@@ -1,8 +1,8 @@
 package msateam;
 
 import msateam.config.kafka.KafkaProcessor;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
+//import com.fasterxml.jackson.databind.DeserializationFeature;
+//import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.messaging.handler.annotation.Payload;
@@ -10,10 +10,11 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class PolicyHandler{
-    @Autowired MessageRepository messageRepository;
+    @Autowired
+    private MessageRepository messageRepository;
 
     @StreamListener(KafkaProcessor.INPUT)
-    public void whatever(@Payload String eventString){}
+    public void onStringEventListener(@Payload String eventString){}
 
     @StreamListener(KafkaProcessor.INPUT)
     public void wheneverReservationConfirmed_SendConfirmMsg(@Payload ReservationConfirmed reservationConfirmed){
